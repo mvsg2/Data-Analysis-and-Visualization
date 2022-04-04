84fb4a1a8f2085c1b4ceff209b0d5b83b3ee3a37
mvsg2 <89340753+mvsg2@users.noreply.github.com>
2022-04-04 11:52:15 +0530

    Update prime_numbers.py

--- prime_numbers.py
+++ prime_numbers.py
@@ -1,1 +1,12 @@
 # I want to add some code here that has a bunch of instructions to pin-point what the primes are...
+def get_primes():
+  lower_limit = int(input("Enter lower range: "))  
+  upper_limit = int(input("Enter upper range: "))  
+  
+  for num in range(lower_limit,upper_limit + 1):  
+   if num > 1:  
+       for i in range(2,num):  
+           if (num % i) == 0:  
+               break  
+       else:  
+           print(num)  
